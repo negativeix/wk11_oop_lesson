084f102372e72de998f08011456b4325a4f682c8
Chitiwat Phajan <chitiwat2547@gmail.com>
2024-11-09 20:33:40 +0700

    making class Table and TableDB

--- data_processing.py
+++ data_processing.py
@@ -3,55 +3,62 @@ import csv, os
 __location__ = os.path.realpath(
 os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-cities = []
-with open(os.path.join(__location__, 'Cities.csv')) as f:
-    rows = csv.DictReader(f)
-    for r in rows:
-        cities.append(dict(r))
+class TableDB:
+    def __init__(self):
+        self.table_database = []
+        self.table = []
 
-countries = []
-with open(os.path.join(__location__, 'Countries.csv')) as f:
-    rows = csv.DictReader(f)
-    for r in rows:
-        countries.append(dict(r))
+    def insert(self, table):
+        with open(os.path.join(__location__, table)) as f:
+            reader = csv.DictReader(f)
+            self.table_database = list(reader)
 
-# Print the average temperature of all the cities
-print("The average temperature of all the cities:")
-temps = []
-for city in cities:
-    temps.append(float(city['temperature']))
-print(sum(temps)/len(temps))
-print()
+    def search(self, table_name):
+        pass
+class Table:
+    def __init__(self, table_name, table):
+        self.table_name = table_name
+        self.cur = TableDB()
+        self.cur.insert(table)
+        self.table_database = self.cur.table_database
+        self.table = []
 
-# Print all cities in Italy
-temps = []
-my_country = 'Italy'
-for city in cities:
-    if city['country'] == my_country:
-        temps.append(city['city'])
-print("All the cities in", my_country, ":")
-print(temps)
-print()
+    def filter(self, condition):
+        for item in self.table_database:
+            if condition(item):
+                self.table.append(item)
+        return self.table
 
-def filter(condition, dict_list):
-    filtered_list = []
-    for item in dict_list:
-        if condition(item):
-            filtered_list.append(item)
-    return filtered_list
+    def aggregate(self, aggregation_function, aggregation_key):
+        aggregated_list = [float(i[aggregation_key]) for i in self.table]
+        print (aggregation_function(aggregated_list))
+
+    def __str__(self):
+        return ('Table: '+str(self.table)+'\n'
+                +'Table Name: '+str(self.table_name))
 
 
-def aggregate(aggregation_key, aggregation_function, dict_list):
-    aggregated_list = [float(i[aggregation_key]) for i in dict_list]
-    print(aggregation_function(aggregated_list))
+x = Table('Italy','Cities.csv')
+s = x.table_name
+x.filter(lambda x: x['country'] == s)
+x.aggregate((lambda x:sum(x)/len(x)),'temperature')
+print(x)
 
+
+
+
+
+
+
+
+# print(y)
 
 # Let's write code to
 # - print the average temperature for all the cities in Italy
-aggregate ('temperature',(lambda x:sum(x)/len(x)), filter( lambda x: x['country'] == 'Italy', cities))
+#aggregate ('temperature',(lambda x:sum(x)/len(x)), filter( lambda x: x['country'] == 'Italy', cities))
 # - print the average temperature for all the cities in Sweden
-aggregate ('temperature',(lambda x:sum(x)/len(x)), filter(lambda x: x['country'] == 'Sweden', cities))
+#aggregate ('temperature',(lambda x:sum(x)/len(x)), filter(lambda x: x['country'] == 'Sweden', cities))
 # - print the min temperature for all the cities in Italy
-aggregate ('temperature',(lambda x:min(x)), filter(lambda x: x['country'] == 'Italy', cities))
+#aggregate ('temperature',(lambda x:min(x)), filter(lambda x: x['country'] == 'Italy', cities))
 # - print the max temperature for all the cities in Sweden
-aggregate ('temperature',(lambda x:max(x)), filter(lambda x: x['country'] == 'Sweden', cities))
+#aggregate ('temperature',(lambda x:max(x)), filter(lambda x: x['country'] == 'Sweden', cities))
